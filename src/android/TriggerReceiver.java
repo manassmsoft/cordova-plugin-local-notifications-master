485e1cd4ebac0d16a0350a1f6413fb00ce9fa65c
manassmsoft <34936440+manassmsoft@users.noreply.github.com>
2018-11-20 17:34:05 +0530

    Update TriggerReceiver.java

--- src/android/TriggerReceiver.java
+++ src/android/TriggerReceiver.java
@@ -110,18 +110,18 @@ public class TriggerReceiver extends AbstractTriggerReceiver {
 			
 	       
 	             
-                    String urlParameters  = "lat=abc&lon=manass";
-byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-int    postDataLength = postData.length;
-String request        = "http://shopno33.atspace.cc/writelat.php";
-URL    url            = new URL( request );
+                    String urlParameters="lat=abc&lon=manass";
+byte[] postData=urlParameters.getBytes(StandardCharsets.UTF_8);
+int postDataLength=postData.length;
+String request="http://shopno33.atspace.cc/writelat.php";
+URL url= new URL(request);
 HttpURLConnection conn= (HttpURLConnection) url.openConnection();           
-conn.setDoOutput( true );
-conn.setInstanceFollowRedirects( false );
-conn.setRequestMethod( "POST" );
-conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded"); 
-conn.setRequestProperty( "charset", "utf-8");
-conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+conn.setDoOutput(true);
+conn.setInstanceFollowRedirects(false);
+conn.setRequestMethod("POST");
+conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded"); 
+conn.setRequestProperty("charset","utf-8");
+conn.setRequestProperty("Content-Length",Integer.toString(postDataLength));
 conn.setUseCaches(false);
 DataOutputStream wr = new DataOutputStream(conn.getOutputStream()); 
    wr.write(postData);
